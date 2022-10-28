04167289b89e5732eda2909b11463152400c0425
Oleg Krohmal <krohmal_oleg86@mail.ru>
2022-10-28 23:14:44 +0300

    JsDz6DOP

---
+++ main.js
@@ -0,0 +1,9 @@
+const task6 = () => {
+  const list = Array.from(document.querySelectorAll("li.list-item"));
+  return list
+    .filter((li) => li.classList.contains(li.textContent))
+};
+ 
+console.log(task6());
+
+
