4113f12ed77c90c7812876726556eaf23d429f3c
Ol-wy <115230468+Ol-wy@users.noreply.github.com>
2022-10-28 20:21:18 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const task6 = () => {
-  const list = Array.from(document.querySelectorAll("li.list-item"));
-  return list
-    .filter((li) => li.classList.contains(li.textContent))
-};
+// const task6 = () => {
+//   const list = Array.from(document.querySelectorAll("li.list-item"));
+//   return list
+//     .filter((li) => li.classList.contains(li.textContent))
+// };
  
-console.log(task6());
+// console.log(task6());
 
-
+// const task6 = () => {
+//   const list = Array.from(document.querySelectorAll("li.list-item"));
+//   return list
+//     .filter((li) => !li.classList.contains(li.textContent))
+// };
+ 
+// console.log(task6());
